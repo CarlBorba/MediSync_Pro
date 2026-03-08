81c16e8b9eea997978a54a530c5ccbe35a872f93
CarlosBusiness-exe <business10carlos@gmail.com>
2026-03-08 08:51:48 -0300

    feat: implemented complete appointment schemas unit tests

--- tests/test_appointment_schema.py
+++ tests/test_appointment_schema.py
@@ -2,9 +2,9 @@ import pytest
 from pydantic import ValidationError
 from datetime import date, time
 
-from schemas.appointment_schema import AppointmentSchemaBase
+from schemas.appointment_schema import AppointmentSchemaBase, AppointmentSchemaResponse
 
-def test_appointment_schema_valid():
+def test_ap_schema_valid():
     data = {
         "ap_date":"2026-03-10",
         "ap_time":"14:00:00",
@@ -17,3 +17,39 @@ def test_appointment_schema_valid():
 
     assert appointment.doctor_id == 1
     assert isinstance(appointment.ap_date, date)
+
+def test_ap_schema_invalid_types():
+    data = {
+        "ap_date":"string",
+        "ap_time":"14:00:00",
+        "doctor_id":"string",
+        "patient_id":1,
+        "status":0
+    }
+
+    with pytest.raises(ValidationError):
+        AppointmentSchemaBase(**data)
+
+def test_ap_schema_missing_data():
+    data = {
+        "ap_date":"2026-03-10",
+        "ap_time":"14:00:00",
+        "status":"Pending"
+    }
+
+    with pytest.raises(ValidationError):
+        AppointmentSchemaBase(**data)
+
+def test_ap_schema_response():
+    data = {
+        "id":99,
+        "ap_date":"2026-03-10",
+        "ap_time":"14:00:00",
+        "doctor_id":1,
+        "patient_id":1,
+        "status":"Pending"
+    }
+
+    response = AppointmentSchemaResponse(**data)
+
+    assert response.id == 99
